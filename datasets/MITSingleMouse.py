9650db9509cc6698cfeac6c6b847bebfa23096c6
Ariel <ariel.iporre.rivas@gmail.com>
2021-03-05 21:54:40 +0100

    normalize sample based min/max to 1

--- datasets/MITSingleMouse.py
+++ datasets/MITSingleMouse.py
@@ -1,5 +1,7 @@
 from dpipe.utils import get_video_length, get_read_fcn
 
+
 def read_sample(sample_path):
     read_video_fcn = get_read_fcn('video')
-    return read_video_fcn(sample_path)
+    sample = read_video_fcn(sample_path).astype(float)
+    return sample/sample.max()
