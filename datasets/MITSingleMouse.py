ace144b5a47e50d57e3f05b6759898bd9faa295b
Ariel <ariel.iporre.rivas@gmail.com>
2021-03-06 04:44:40 +0100

    add create_dataset for MIT single mouse videos

--- datasets/MITSingleMouse.py
+++ datasets/MITSingleMouse.py
@@ -1,7 +1,46 @@
-from dpipe.utils import get_video_length, get_read_fcn
+from pathlib import Path
 
+from dpipe.utils import get_video_length, get_read_fcn
+from dpipe import make_dataset, from_function
+from os import path
+
+label_maps = {"drink" : 0,
+    "d" : 0,
+    "eat" : 1,
+    "e" : 1,
+    "groomback" : 2,
+    "groom" : 2,
+    "gb" : 2,
+    "g" : 2,
+    "hang" : 3,
+    "ha" : 3,
+    "micromovement" : 4,
+    "head" : 4,
+    "he" : 4,
+    "rear": 5,
+    "r" : 5,
+    "rest" : 6,
+    "rs" : 6,
+    "walk" : 7,
+    "w" : 7
+}
 
 def read_sample(sample_path):
     read_video_fcn = get_read_fcn('video')
     sample = read_video_fcn(sample_path).astype(float)
-    return sample/sample.max()
+    return (sample-sample.min())/(sample.max()-sample.min())
+
+def create_dataset(dataset_path, with_labels=False):
+    files = list(map(lambda x: x.as_posix(), Path(dataset_path).rglob('*.mpg')))
+    if with_labels:
+        labels = list(map(lambda x: label_maps[path.basename(x).split('_')[-2]], files))
+        def read_sample_with_labels(inputs):
+            path, label = inputs
+            video = read_sample(path)
+            return video, label
+        files_with_labels = list(zip(files, labels))
+        dataset = from_function(read_sample_with_labels, files_with_labels, undetermined_shape=[0])
+    else:
+        dataset = from_function(read_sample, files, undetermined_shape=[0])
+    return dataset
+
--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,1 +1,2 @@
 from .MITSingleMouse import read_sample as mit_single_mouse_read_sample
+from .MITSingleMouse import create_dataset as  mit_single_mouse_create_dataset
