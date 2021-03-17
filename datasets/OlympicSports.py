9d39584ae9c1b404189447c74e4e39ef23d5c75c
Ariel <ariel.iporre.rivas@gmail.com>
2021-03-17 19:40:02 +0100

    created olympic sports dataset

---
+++ datasets/OlympicSports.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from dpipe import from_function
+from os import path
+from random import shuffle as shuffle_list
+from olympic_sports import read_seq
+import numpy as np
+
+def normalize(x):
+    return (x - x.min()) / (x.max() - x.min())
+
+def read_sample(sample_path):
+    frames = read_seq(sample_path)
+    sample = np.array(frames, dtype=np.float32)
+    return normalize(sample)
+
+
+def create_dataset(dataset_path, with_labels=False, shuffle=False):
+    files = list(map(lambda x: x.as_posix(), Path(dataset_path).rglob('*.seq')))
+    if shuffle:
+        shuffle_list(files)
+    if with_labels:
+        labels = list(map(lambda x: path.basename(path.dirname(x)), files))
+        print(labels)
+        def read_sample_with_labels(inputs):
+            path, label = inputs
+            video = read_sample(path)
+            return video, label
+        files_with_labels = list(zip(files, labels))
+        dataset = from_function(read_sample_with_labels, files_with_labels, undetermined_shape=[[0],[]])
+    else:
+        dataset = from_function(read_sample, files, undetermined_shape=[0])
+    return dataset
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    ds = create_dataset(dataset_path='../data/human', with_labels=False).build()
+    for d in ds:
+        print(d.shape)
+        plt.imshow(d[0])
+        plt.show()
